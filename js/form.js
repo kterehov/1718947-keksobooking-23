00acd604e3ad37edc645e0526a3d316af303ec4e
Константин Терехов <konstantinterehov@iMac-Konstantin.local>
2021-06-25 12:19:39 +0300

    fix

--- js/form.js
+++ js/form.js
@@ -3,7 +3,7 @@ const interactiveEl = {
     '.map__filter',
     '.map__features',
   ],
-  '.ad-form' : [
+  '.ad-form': [
     '.ad-form-header__input',
     '.ad-form__element',
   ],
@@ -17,11 +17,23 @@ const interactiveEl = {
 const toggleForms = (disable = true) => {
   for (const selectorForm in interactiveEl) {
     const currentForm = document.querySelector(selectorForm);
-    disable ? currentForm.classList.add('ad-form--disabled') : currentForm.classList.remove('ad-form--disabled');
+
+    if (disable) {
+      currentForm.classList.add('ad-form--disabled');
+    } else {
+      currentForm.classList.remove('ad-form--disabled');
+    }
+
     for (const selectorElements in interactiveEl[selectorForm]) {
       const currentElements = currentForm.querySelectorAll(interactiveEl[selectorForm][selectorElements]);
       currentElements.forEach((element) => {
-        disable ? element.setAttribute('disabled', 'disabled') : element.removeAttribute('disabled');
+
+        if (disable) {
+          element.setAttribute('disabled', 'disabled');
+        } else {
+          element.removeAttribute('disabled');
+        }
+
       });
     }
   }
